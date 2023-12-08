0c08c7e974afe3d71fde43020be4ac03f47d1a2a
Gonzalo Fernandez <ezekyel34@gmail.com>
2023-12-08 18:17:03 -0300

    pequeña modificación

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-// function scrollToElement(elementSelector, instance = 0) {
-//     // Select all elements that match the given selector
-//     const elements = document.querySelectorAll(elementSelector);
-//     // Check if there are elements matching the selector and if the requested instance exists
-//     if(elements.length > instance) {
-//         // Scroll to the specified instance of the element
-//         elements[instance].scrollIntoView({ behavior: 'smooth' });
-//     }
-// }
-
-// const link1 = document.getElementById("link1"); //about
-// const link2 = document.getElementById("link2"); //targets
-// const link3 = document.getElementById("link3"); // projects
-
-// link1.addEventListener('click', () => {
-//     scrollToElement('.about');
-// });
-
 function scrollToElement(elementSelector, instance = 0) {
     // Select all elements that match the given selector
     const elements = document.querySelectorAll(elementSelector);
